312140fb89ccdecca66842615a0211256f74a045
guptaabhi95 <guptaabhi95@gmail.com>
2021-12-27 14:38:05 +0530

    First Commit

---
+++ src/main/java/com/learnhibernate/abhinav/AbhinavApplication.java
@@ -0,0 +1,13 @@
+package com.learnhibernate.abhinav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhinavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhinavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnhibernate/abhinav/AbhinavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnhibernate.abhinav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhinavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
